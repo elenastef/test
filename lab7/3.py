84da68507784c79da352fb7e8f82c4d1b86eba44
Elena Stefanova <elena.stefanovabg@gmail.com>
2016-12-12 00:06:50 -0600

    Add files via upload

--- lab7/3.py
+++ lab7/3.py
@@ -5,11 +5,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NameOfaFaculty
 import unittest, time, re
 
-
-class Stefanovalab7(unittest.TestCase):
+class 3(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -17,14 +15,13 @@ class Stefanovalab7(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_stefanovalab7(self):
+    def test_3(self):
         driver = self.driver
         driver.get(self.base_url + "/")
         driver.find_element_by_link_text("Employee Directory").click()
         driver.find_element_by_id("name").clear()
         driver.find_element_by_id("name").send_keys("William Tucker")
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-        
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
--- lab8/StefanovaLab8.py
+++ lab8/StefanovaLab8.py
@@ -5,11 +5,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NameOfaFaculty
 import unittest, time, re
 
-
-class Stefanovalab7(unittest.TestCase):
+class StefanovaLab8(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -17,14 +15,18 @@ class Stefanovalab7(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_stefanovalab7(self):
+    def test_stefanova_lab8(self):
         driver = self.driver
-        driver.get(self.base_url + "/")
-        driver.find_element_by_link_text("Employee Directory").click()
-        driver.find_element_by_id("name").clear()
-        driver.find_element_by_id("name").send_keys("William Tucker")
-        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-        
+        driver.get(self.base_url + "/calendars/academic-calendar")
+        driver.find_element_by_xpath("//img[contains(@src,'http://www.austincc.edu/sites/default/files/menuicon.jpg')]").click()
+        driver.find_element_by_css_selector("li.leaf.menu-mlid-8397 > a").click()
+        driver.find_element_by_link_text("Assessment").click()
+        driver.find_element_by_link_text("Libraries").click()
+        driver.find_element_by_link_text("Career Center").click()
+        driver.find_element_by_link_text("Departments & Divisions").click()
+        Select(driver.find_element_by_id("edit-field-instructional-division-value")).select_by_visible_text("Adult Education")
+        Select(driver.find_element_by_id("edit-field-instructional-title-value")).select_by_visible_text("Dean")
+        driver.find_element_by_link_text("Tasha Davis").click()
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
